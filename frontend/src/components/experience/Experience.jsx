321ab662c3aa5bbdfa56a3dfd17a3b8b0568f1ae
Chitchai-Jantanarak <s6604062663051@email.kmutnb.ac.th>
2024-12-23 21:53:53 +0700

    Commit 12-23

--- frontend/src/components/experience/Experience.jsx
+++ frontend/src/components/experience/Experience.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
-import { Environment, OrbitControls } from "@react-three/drei";
+import React, { useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, PerspectiveCamera, Text, ScrollControls, Scroll, OrbitControls } from "@react-three/drei";
 import { Cloud } from "../models/Cloud";
+import { Fonts } from "../../utils/Fonts";
 
 const Experience = () => {
 
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile]   = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);
+            setIsMobile(window.innerWidth <= 768);
         }
         handleResize();
         window.addEventListener('resize', handleResize);
@@ -17,21 +18,42 @@ const Experience = () => {
     }, []);
 
     return (
+        <>
         <Canvas 
-            gl={{ alpha: true }} 
+            gl={{ alpha: true }}
             style={{ 
-                // background: '#F0F1CB', 
                 background: 'linear-gradient(to bottom, #74c3fb 10%, #f0f1cb 10%, #f0f1cb 90%, #74c3fb 90%)',
                 width: '100%', 
                 height: '100%' 
             }}
         >
-            <Environment preset='dawn' /> 
+            <ScrollControls pages={2} damping={0.5}>
+                <Environment preset='dawn' /> 
+                <ambientLight intensity={0.4} />
+                <PerspectiveCamera makeDefault position={[0, 0, 9]} fov={isMobile ? 75 : 45} />
+                <OrbitControls></OrbitControls>
+                <Text
+                    position={[0, 2.5, -1]}
+                    fontSize={isMobile ? 0.75 : 1}
+                    font={Fonts.header}
+                    color="brown"
+                    anchorX="center"
+                    anchorY="middle"
+                    letterSpacing={0.4}
+                >
+                    YADOMMMM
+                </Text>
 
-            <Cloud position={[0, 0, 0]} scale={isMobile ? 1 : 2} />
-            <OrbitControls />
+                <Cloud position={[0, 0, 1]}         rotation={[321, 20, 0]}     scale={isMobile ? 1.25 : 1.5} />
+                <Cloud position={[-5, 1, 1]}        rotation={[60, 180, 0]}     scale={isMobile ? 1.25 : 1.5} />
+                <Cloud position={[-5, -2, 1]}       rotation={[0, 0, 0]}        scale={isMobile ? 1.25 : 1.5} />
+                <Cloud position={[-1.5, -2.25, -1]} rotation={[0, 180, 0]}      scale={isMobile ? 1.25 : 1.5} />
+                <Cloud position={[3, -2, 1]}        rotation={[320, 45, 0]}     scale={isMobile ? 1.25 : 1.5} />
+                <Cloud position={[5, 3, 0]}         rotation={[0, 0, 0]}        scale={isMobile ? 1.25 : 1.5} />
+                
+            </ScrollControls>
         </Canvas>
-
+        </>
     );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'noto-sans-thai': ['Noto Sans Thai', 'sans-serif'],
+          'poppins': ['Poppins', 'sans-serif'],
+          'alike': ['Alike', 'serif'],
+          'concert-one': ['Concert One', 'cursive'],
+        },
+      },
     },
     plugins: [
       require('daisyui'),
